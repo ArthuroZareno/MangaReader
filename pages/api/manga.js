15fb06c56a36deadc7fc486bbd2e19297055acb3
ArthuroZareno <jope.zareno.up@phinmaed.com>
2025-02-17 20:13:07 +0800

    Fix CORS issues

--- pages/api/manga.js
+++ pages/api/manga.js
@@ -1,20 +1,18 @@
 export default async function handler(req, res) {
-    try {
-      const response = await fetch("https://api.mangadex.org/manga?limit=10", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error("Failed to fetch manga data");
-      }
-  
-      const data = await response.json();
-      res.status(200).json(data);
-      res.setHeader("Access-Control-Allow-Origin", "*");
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+  try {
+    const response = await fetch("https://api.mangadex.org/manga?limit=10", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch manga data");
     }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  
+}
